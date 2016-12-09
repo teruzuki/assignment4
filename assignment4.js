a639387b0443bc3192b72c699aa4ff82ac7f2785
Yuefu Jiang <yuj19@pitt.edu>
2016-12-08 22:58:14 -0500

    Update

--- assignment4.js
+++ assignment4.js
@@ -9,10 +9,36 @@
 // $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
 // Add content from requests with something like
 // $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
-var address = "http://www.mattbowytz.com/simple_api.json?data=all"
+// keyup events could be helpful to get value of field as the user type
+ (function(){
+ 	$("#result").hide();
+ })();
 
-(function() {
-  // Magic!
-  //notyet
-}
+ $(function() {
+  $(".flexsearch-input").on("input", function() {
+      var myInput = $(".flexsearch-input").val();
+      if(myInput.length > 0){
+        $("#results").show();
+        $.ajax({
+          dataType: "json",
+          url: "http://www.mattbowytz.com/simple_api.json?data=all",
+          success: function(outResult){
+            $.each(outResult.data, function(key1, value1){
+              $.each(value1, function(key2, value2){
+
+              	valueLC = value2.toLowerCase();
+
+                if(valueLC.search(myInput) > -1){
+                  $("#results").append("<li>"+value2+"</li>");
+                }
+              })
+            })
+          }
+        });
+      }
+      else {
+        $("#results").html("");
+        $("#results").hide();
+      }
+  });
+ });
